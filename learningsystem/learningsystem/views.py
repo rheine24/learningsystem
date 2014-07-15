43a90cbfa2b91d8282fd5575359ed629566db25c
Marc Francisco <rheine24@github.com>
2014-07-15 11:11:17 +0800

    Edit views.py

--- learningsystem/learningsystem/views.py
+++ learningsystem/learningsystem/views.py
@@ -1,4 +1,12 @@
+from django.shortcuts import render
+
 from django.http import HttpResponse
 
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 def home(request):
-    return HttpResponse("HOME PAGE")
+	context = RequestContext(request)
+	context_dict = {'boldmessage':'BOLD MESSAGE'}
+
+	return render_to_response('login.html',context_dict, context)

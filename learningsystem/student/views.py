d4a0cdb70607c7e3135f1a838c5a0e6997734963
Marc Francisco <rheine24@github.com>
2014-07-17 13:30:33 +0800

    Integrated login

--- learningsystem/student/views.py
+++ learningsystem/student/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
 	context = RequestContext(request)
-	context_dict = {'boldmessage':'BOLD MESSAGE'}
+	context_dict = {'boldmessage':'Failed Login'}
+	if request.method == 'POST':
+		user = authenticate(username = request.POST['userid'], password = request.POST['password'])
+		if user is not None:
+			return redirect('test/')
 
 	return render_to_response('student/index.html',context_dict, context)
+
+def test(request):
+	return HttpResponse("HELLO WORLD")

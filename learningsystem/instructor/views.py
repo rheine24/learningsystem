aecfa3edcc9c3edf76bb4e97cbfbe2e19479656c
kjcmanabat <kjcmanabat@gmail.com>
2014-07-23 11:25:15 +0800

    resized blurred bg - login

--- learningsystem/instructor/views.py
+++ learningsystem/instructor/views.py
@@ -12,6 +12,6 @@ def home(request):
 	context = RequestContext(request)
 	context_dict = {'boldmessage':'Login Success'}
 	return render_to_response('instructor/inst1.html',context_dict, context)
-
+	
 
 #C:\Users\KatherineJoy\Desktop\workspace\for django\learningsystem\learningsystem\templates\instructor

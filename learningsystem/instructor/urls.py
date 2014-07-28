0c19ccff75fd1510b464758a6add48b99b6773f7
Marc Francisco <rheine24@github.com>
2014-07-29 07:48:45 +0800

    Fixed inst.html

--- learningsystem/instructor/urls.py
+++ learningsystem/instructor/urls.py
@@ -3,6 +3,9 @@ from instructor import views
 
 urlpatterns = patterns('',
         url(r'^$', views.home, name='InsIndex'),
-        url(r'^home/', views.home, name = 'InsHome')
+        url(r'^home/', views.home, name = 'InsHome'),
+        url(r'^confirmevent/',views.confirmEvent, name = 'InsConfirm'),
+        url(r'^saveevent/',views.saveEvent, name = 'InsSave')
+
              
         )
--- learningsystem/instructor/views.py
+++ learningsystem/instructor/views.py
@@ -12,8 +12,46 @@ from datetime import *
 def home(request):
 	context = RequestContext(request)
 	context_dict = {'boldmessage':'Login Success', 'vdateandtime':datetime.today(),
-					'instTabChecker':0}
+					'instTabChecker':0,'request':''}
+
+	if request.method == 'GET':
+		context_dict['request'] = request.user 
+
+	if request.method == 'POST':
+		context_dict['request'] = request.POST
 	return render_to_response('instructor/inst.html',context_dict, context)
 	
+def confirmEvent(request):
+	context = RequestContext(request)
+	context_dict = {'vdateandtime':datetime.today(),'instTabChecker':1,
+					'request':'',}
+
+	if request.method == 'GET':
+		context_dict['request'] = request.GET
+
+	if request.method == 'POST':
+		context_dict['request'] = request.POST
+		context_dict['vConfirmEventName'] = request.POST.get('eventName')
+		context_dict['vConfirmSchedule'] = request.POST.get('schedule')
+		context_dict['vConfirmStartTime'] = request.POST.get('startTime')
+		context_dict['vConfirmEndTime'] = request.POST.get('endTime')
+		context_dict['vConfirmFrequency'] = request.POST.get('frequency')
+		context_dict['vConfirmEnlistment'] = request.POST.get('enlistmentType')
+		context_dict['vConfirmDescription'] = request.POST.get('description')
+
+	return render_to_response('instructor/inst.html',context_dict,context)
+
+def saveEvent(request):
+	context = RequestContext(request)
+	context_dict = {'vdateandtime':datetime.today(),'instTabChecker':2,
+					'request':''}
+
+	if request.method == 'GET':
+		context_dict['request'] = request.GET
+
+	if request.method == 'POST':
+		context_dict['request'] = request.POST
+
+	return render_to_response('instructor/inst.html',context_dict,context)
 
 #C:\Users\KatherineJoy\Desktop\workspace\for django\learningsystem\learningsystem\templates\instructor
